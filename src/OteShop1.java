6a73e0ad091fa520fc319359c8f7a8b394ca174d
tvasilad <doris.vasil@gmail.com>
2024-10-08 16:09:23 +0300

    first

---
+++ src/OteShop1.java
@@ -0,0 +1,13 @@
+public class OteShop1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        Customer customer = new Customer();
+        customer.setAddress("Athens");
+
+        Customer customer2 = new Customer();
+        customer2.setAddress("Berlin");
+        System.out.println(customer2.getAddress());
+
+    }
+}

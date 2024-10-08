9b660fe1ce7523bce379c68f7a8642ae6c3479f4
tvasilad <doris.vasil@gmail.com>
2024-10-08 16:34:31 +0300

    test new

--- src/OteShop1.java
+++ src/OteShop1.java
@@ -4,14 +4,26 @@ public class OteShop1 {
 
         Customer customer = new Customer();
         customer.setAddress("Athens");
+        customer.setSurname("Irac");
+
+        Product product = new Product();
+        product.setPrice(20.00);
+        product.setName("chips");
+
+        double fpa = 0.24;
+        Payment payment = new Payment();
+        payment.setCustomer(customer);
+        payment.setAmount(product.getPrice()*(1+fpa));
+
+        System.out.println(payment.getAmount());
+
 
         Customer customer2 = new Customer();
         customer2.setAddress("Berlin");
         System.out.println(customer2.getAddress());
 
-        fgfgf
-                kdkdkdk
-                
+
+
 
     }
 }

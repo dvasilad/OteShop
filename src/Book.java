2dba32443bf683e6bd443e7998a09095f7b7b452
tvasilad <doris.vasil@gmail.com>
2024-10-09 11:57:31 +0300

    test new insert pass class Utilities class book

---
+++ src/Book.java
@@ -0,0 +1,36 @@
+public class Book {
+    //attributes, constructors, setters/getters
+    private String summary;
+    private int numberOfPages;
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+    
+        //repeating characters
+        public boolean isRepeatingSummary() {
+            return Utilities.getFirstRepeatedCharacter(this.summary) != -1;
+        }
+
+
+    //method showPagesToRead
+        public void showPagesToRead() {
+        int pages = 0;
+        int middlePage = pages / 2;
+        System.out.println("Starting from page: " + middlePage);
+        Utilities.goDownToOne(middlePage);
+    }
+
+}
